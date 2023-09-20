8083692d47a34324d33812eeb05db06f19c5dc95
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-09-20 21:42:16 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Отримуємо список ul#ingredients
+const list = document.getElementById('ingredients');
+
+// Створюємо масив елементів li
+const items = [];
+
+// Для кожного елемента масиву ingredients
+ingredients.forEach(ingredient => {
+  // Створюємо елемент li
+  const item = document.createElement('li');
+  // Додаємо назву інгредієнта як його текстовий вміст
+  item.textContent = ingredient;
+  // Додаємо елементу клас item
+  item.classList.add('item');
+  // Додаємо елемент li до масиву items
+  items.push(item);
+});
+
+// Вставляємо усі елементи li за одну операцію у список ul#ingredients
+list.append(...items);
+console.log(list);
+console.log(list.innerHTML);
